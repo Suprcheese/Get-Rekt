241c9c4188f8cf888cb8002fd548ecb8de1391fd
Suprcheese <Suprcheese@users.noreply.github.com>
2018-02-01 01:05:22 +0000

    Handle extending already-jammed turrets

--- control.lua
+++ control.lua
@@ -11,14 +11,14 @@ function process_tick()
 	for i = #global.jammed_turrets, 1, -1 do -- Loop over table backwards because some entries get removed within the loop
 		local vehicle = global.jammed_turrets[i][1]
 		if vehicle and vehicle.valid then
-			if current_tick == global.jammed_turrets[i][3] - 84 then
+			if current_tick == global.jammed_turrets[i][3] - 84 then -- Synchronize sound with actual unjamming time
 				vehicle.surface.play_sound{path = "turret-repaired", position = vehicle.position}
 				vehicle.relative_turret_orientation = global.jammed_turrets[i][2]
 			elseif current_tick == global.jammed_turrets[i][3] then
 				vehicle.surface.create_entity{name = "tutorial-flying-text", position = vehicle.position, text = {"turret-repaired"}}
 				table.remove(global.jammed_turrets, i)
 			else
-				vehicle.relative_turret_orientation = global.jammed_turrets[i][2]
+				vehicle.relative_turret_orientation = global.jammed_turrets[i][2] -- Can still fire the guns, just only in the one direction; can't turn the turret
 			end
 		else
 			table.remove(global.jammed_turrets, i)
@@ -36,8 +36,7 @@ script.on_event(defines.events.on_entity_damaged, function(event)
 	-- if entity.type == "car" and event.force and not event.force.name == "neutral" then
 		local damage_ratio = event.original_damage_amount / entity.prototype.max_health
 		if damage_ratio > 0.1 then
-			game.players[1].print("Beep1!")
-			-- if math.random(7) == 7 then
+			-- if math.random(5) == 5 then
 			if true then
 				applyMalfunction(entity, math.random(2))
 			end
@@ -47,14 +46,21 @@ end)
 
 function applyMalfunction(vehicle, type)
 	if type == 1 then
-		-- vehicle.surface.create_entity({name = "vehicle-damage-projectile", target = vehicle, force = game.forces.enemy, position = vehicle.position, speed = 1})
+		vehicle.surface.create_entity({name = "vehicle-damage-explosion", position = vehicle.position})
 		global.jammed_turrets = global.jammed_turrets or {}
-		table.insert(global.jammed_turrets, {vehicle, vehicle.relative_turret_orientation, game.tick + 5 * 60 + math.random(10 * 60)})
-		vehicle.surface.create_entity{name = "tutorial-flying-text", position = vehicle.position, text = {"turret-jammed"}, color = {r = 1}}
-		script.on_event(defines.events.on_tick, process_tick)
-		game.players[1].print("Beep2!")
-		return
+		local i = findEntity(vehicle)
+		if i then
+			global.jammed_turrets[i][3] = global.jammed_turrets[i][3] + 60 + math.random(5 * 60) -- Extend jammed duration a bit
+			return
+		else
+			table.insert(global.jammed_turrets, {vehicle, vehicle.relative_turret_orientation, game.tick + 5 * 60 + math.random(10 * 60)})
+			vehicle.surface.create_entity{name = "tutorial-flying-text", position = vehicle.position, text = {"turret-jammed"}, color = {r = 1}}
+			script.on_event(defines.events.on_tick, process_tick)
+			game.players[1].print("Turret jammed!")
+			return
+		end
 	elseif type == 2 then
+		game.players[1].print("Sticker!")
 		vehicle.surface.create_entity({name = "vehicle-damage-slowdown-projectile", target = vehicle, force = game.forces.enemy, position = vehicle.position, speed = 1})
 		return
 	end
@@ -65,20 +71,20 @@ script.on_event(defines.events.on_player_changed_position, function(event)
 	local vehicle = player.vehicle
 	if vehicle and vehicle.valid and vehicle.type == "car" then
 		local health_ratio = vehicle.health / vehicle.prototype.max_health
-		if health_ratio >= 0.85 then
+		if health_ratio >= 0.85 then -- (Near-)Full health, no penalties
 			vehicle.effectivity_modifier = 1
 			vehicle.friction_modifier = 1
 			return
-		elseif health_ratio < 0.85 then
+		elseif health_ratio < 0.85 then	-- Linearly scaling penalty based on health
 			health_ratio = health_ratio + 0.15
 			vehicle.effectivity_modifier = health_ratio
 			vehicle.friction_modifier = 2 - health_ratio
-			makeRattleNoise(player, math.random(10))
+			-- makeRattleNoise(player, math.random(10))
 			return
-		elseif health_ratio < 0.1 then
+		elseif health_ratio < 0.1 then -- Near-dead, maximum penalties applied with no further scaling based on health
 			vehicle.effectivity_modifier = 0.25
 			vehicle.friction_modifier = 1.75
-			makeRattleNoise(player, math.random(10))
+			-- makeRattleNoise(player, math.random(10))
 		end
 	end
 end)
@@ -89,14 +95,14 @@ function makeRattleNoise(player, chance)
 	end
 end
 
--- function findEntity(entity)
-	-- for i,t in pairs(global.jammed_turrets) do
-		-- if t[1] == entity then
-			-- return i
-		-- end
-	-- end
-	-- return false
--- end
+function findEntity(entity)
+	for i,t in pairs(global.jammed_turrets) do
+		if t[1] == entity then
+			return i
+		end
+	end
+	return false
+end
 
 -- function entityRemoved(entity)
 	-- local z = findEntity(entity)
--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -14,53 +14,53 @@ local vehicle_slowdown_projectile = util.table.deepcopy(data.raw["projectile"]["
 
 vehicle_slowdown_projectile.name = "vehicle-damage-slowdown-projectile"
 vehicle_slowdown_projectile.acceleration = 1
-vehicle_slowdown_projectile.action =
-{
-	type = "direct",
-	action_delivery =
-	{
-		type = "instant",
-		target_effects =
-		{
-			{
-				type = "create-sticker",
-				entity_name = "vehicle-damaged-sticker"
-			},
-			{
-				type = "create-entity",
-				entity_name = "vehicle-damage-explosion"
-			},
-		}
-	}
-}
-vehicle_slowdown_projectile.light = {}
+-- vehicle_slowdown_projectile.action =
+-- {
+	-- type = "direct",
+	-- action_delivery =
+	-- {
+		-- type = "instant",
+		-- target_effects =
+		-- {
+			-- {
+				-- type = "create-sticker",
+				-- entity_name = "vehicle-damaged-sticker"
+			-- },
+			-- {
+				-- type = "create-entity",
+				-- entity_name = "vehicle-damage-explosion"
+			-- },
+		-- }
+	-- }
+-- }
+vehicle_slowdown_projectile.light = nil
 vehicle_slowdown_projectile.animation = null
 vehicle_slowdown_projectile.shadow = null
 
 local vehicle_damage_explosion = util.table.deepcopy(data.raw["explosion"]["medium-explosion"])
 
 vehicle_damage_explosion.name = "vehicle-damage-explosion"
-vehicle_damage_explosion.animations = null
+vehicle_damage_explosion.animations = {null}
 vehicle_damage_explosion.light = {intensity = 1, size = 10, color = {r=1.0, g=1.0, b=1.0}}
-vehicle_damage_explosion.sound =
-{
-	aggregation =
-	{
-		max_count = 1,
-		remove = true
-	},
-	variations =
-	{
-		{
-			filename = "__base__/sound/fight/large-explosion-1.ogg",
-			volume = 0.8
-		},
-		{
-			filename = "__base__/sound/fight/large-explosion-2.ogg",
-			volume = 0.8
-		}
-	}
-}
+vehicle_damage_explosion.sound = nil
+-- {
+	-- aggregation =
+	-- {
+		-- max_count = 1,
+		-- remove = true
+	-- },
+	-- variations =
+	-- {
+		-- {
+			-- filename = "__base__/sound/fight/large-explosion-1.ogg",
+			-- volume = 0.8
+		-- },
+		-- {
+			-- filename = "__base__/sound/fight/large-explosion-2.ogg",
+			-- volume = 0.8
+		-- }
+	-- }
+-- }
 vehicle_damage_explosion.created_effect =
 {
 	type = "direct",
@@ -71,13 +71,13 @@ vehicle_damage_explosion.created_effect =
 		{
 			{
 				type = "create-particle",
-				repeat_count = 10,
+				repeat_count = 5,
 				entity_name = "explosion-remnants-particle",
-				initial_height = 0.5,
-				speed_from_center = 0.04,
-				speed_from_center_deviation = 0.08,
-				initial_vertical_speed = 0.04,
-				initial_vertical_speed_deviation = 0.08,
+				initial_height = 0.75,
+				speed_from_center = 0.03,
+				speed_from_center_deviation = 0.04,
+				initial_vertical_speed = 0.03,
+				initial_vertical_speed_deviation = 0.04,
 				offset_deviation = {{-0.1, -0.1}, {0.1, 0.1}}
 			}
 		}
@@ -88,8 +88,9 @@ vehicle_damage_explosion.smoke_count = 1
 vehicle_damage_explosion.smoke_slow_down_factor = 1
 
 
-local vehicle_damaged_sticker = util.table.deepcopy(data.raw["sticker"]["vehicle-damaged-sticker"])
+local vehicle_damaged_sticker = util.table.deepcopy(data.raw["sticker"]["slowdown-sticker"])
 
+vehicle_damaged_sticker.name = "vehicle-damaged-sticker"
 vehicle_damaged_sticker.duration_in_ticks = 3 * 60
 
 data:extend({vehicle_slowdown_projectile, vehicle_damage_explosion, vehicle_damaged_sticker})
